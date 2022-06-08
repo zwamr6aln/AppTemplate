e533791e7bee68205e7ea727eb2cd395a9063f3d
aaaa <aaaa>
2022-06-08 09:42:31 +0900

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by nnname on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTemplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppTemplate
+//
+//  Created by nnname on 2022/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
