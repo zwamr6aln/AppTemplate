08856fb55cb0eb132f57d1bef9ce0f89faa88814
aaaa <aaaa>
2022-06-22 10:52:22 +0900

    a

--- AppTemplate/🛠Menu/📄Document/🏷VersionSection.swift
+++ AppTemplate/🛠Menu/📄Document/🏷VersionSection.swift
@@ -7,28 +7,37 @@ struct 🏷VersionSection: View {
             NavigationLink {
                 📋TextView(🕒VersionHistory, "Version History")
             } label: {
-                Label(🕒LatestVersion, systemImage: "signpost.left")
+                Label(🕒LatestVersionNumber, systemImage: "signpost.left")
             }
             .accessibilityLabel("Open version history")
         } header: {
             Text("Version")
         } footer: {
             let 📅 = Date.now.formatted(date: .numeric, time: .omitted)
-            Text(LocalizedStringKey("builded on \(📅)"))
+            Text("builded on \(📅)")
         }
     }
 }
 
-let 🕒LatestVersion = "1.0"
 
-let 🕒VersionHistory = """
-🕒 Version 1.1 : (2022-AA-AA?)
+let 🕒LatestVersionNumber = "1.1"
+
+let 🕒LatestVersionDescription = """
 ==== English description ====
 - AAAA
 ==== Japanese(native) description ====
 - AAAA
+"""
 
+var 🕒VersionHistory: String {
+    var 📃 = "🕒 Version " + 🕒LatestVersionNumber + " : "
+    📃 += "(builded on " + Date.now.formatted(date: .numeric, time: .omitted) + ")\n"
+    📃 += 🕒LatestVersionDescription + "\n\n\n"
+    📃 += 🕒PastVersionHistory
+    return 📃
+}
 
+let 🕒PastVersionHistory = """
 🕒 Version 1.0 : 2022-AA-AA
 Initial release
 """
