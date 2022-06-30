26d785e8a4b36efd866b35395b961574aa708c4a
aaaa <aaaa>
2022-06-30 12:07:38 +0900

    🔩UniqueData.swift

---
+++ AppTemplate/🧰Others/🔩UniqueData.swift
@@ -0,0 +1,54 @@
+
+import Foundation
+
+//Manually sync below text between "🔩UniqueData.swift" and "/README.md(here)" and "AppStoreConnect/AAAA/Description".
+
+let 📄AppStoreDescription = """
+
+placeholder
+
+"""
+
+
+// ======== App Version =========
+//let 🕒LatestVersionNumber = "1.1"
+//
+//let 🕒LatestVersionDescription = """
+//==== English ====
+//- AAAA
+//- Various bugfixes and improvements.
+//==== Japanese(native) ====
+//- ああああ
+//- いくつかの改善やバグ修正を行いました。
+//"""
+//
+//let 🕒PastVersionHistory = """
+//🕒 Version 1.0 : 2022-AA-AA
+//Initial release
+//最初のリリース
+//"""
+
+
+
+
+let 📃PrivacyPolicy = """
+2022-AA-AA
+
+(English)This application don't collect user infomation.
+
+(Japanese)このアプリ自身において、ユーザーの情報を一切収集しません。
+"""
+
+
+
+
+let 🛒InAppPurchaseProductID = "placeholder.adfree"
+
+
+
+
+let 🔗AppStoreProductURL = URL(string: "https://apps.apple.com/app/id1111")!
+
+let 🔗WebRepositoryURL = URL(string: "https://github.com/FlipByBlink/AAAA")!
+
+let 🔗WebRepositoryMirrorURL = URL(string: "https://gitlab.com/FlipByBlink/AAAA_Mirror")!
