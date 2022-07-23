2aa4f0f47cfebb5974d31e520573e652c42e110e
aaaa <aaaa>
2022-07-24 08:57:26 +0900

    ﹀CloseMenuButton

--- AppTemplate/🧰Template/🛠TemplateMenu.swift
+++ AppTemplate/🧰Template/🛠TemplateMenu.swift
@@ -7,26 +7,3 @@ struct 🛠TemplateMenu: View {
         📄InformationMenuLink()
     }
 }
-
-struct ﹀CloseMenuButton: ToolbarContent {
-    @Binding var 🚩ShowMenu: Bool
-    
-    var body: some ToolbarContent {
-        ToolbarItem(placement: .navigationBarTrailing) {
-            Button {
-                🚩ShowMenu = false
-                UISelectionFeedbackGenerator().selectionChanged()
-            } label: {
-                Image(systemName: "chevron.down")
-                    .foregroundStyle(.secondary)
-                    .grayscale(1.0)
-                    .padding(8)
-            }
-            .accessibilityLabel("Dismiss")
-        }
-    }
-    
-    init(_ 🚩: Binding<Bool>) {
-        _🚩ShowMenu = 🚩
-    }
-}
