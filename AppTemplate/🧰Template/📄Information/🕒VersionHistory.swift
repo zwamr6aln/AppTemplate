3625540bf138cd88715a952091e8e5ae43ad0a1a
aaaa <aaaa>
2022-07-24 14:02:44 +0900

    a

--- AppTemplate/🧰Template/📄Information/🕒VersionHistory.swift
+++ AppTemplate/🧰Template/📄Information/🕒VersionHistory.swift
@@ -4,10 +4,10 @@ let 🕒VersionNumber = "2.0"
 let 🕒VersionDescription = """
 AAAA
 Various bugfixes and improvements.
-
 ああああ
 いくつかの改善やバグ修正を行いました。
 """
+
 
 import SwiftUI
 
@@ -44,7 +44,8 @@ struct 🕒VersionSection: View {
     var body: some View {
         Section {
             Text(📃.text)
-                .padding(32)
+                .font(.subheadline)
+                .padding()
         } header: {
             Text(📃.number)
         } footer: {
@@ -58,21 +59,18 @@ struct 🕒VersionSection: View {
     }
 }
 
-typealias 🕒VersionFormat = (number: String, text: String, date: String)
+typealias 🕒VersionFormat = (number: String, text: String, date: LocalizedStringKey)
 
 let 🕒VersionHistoryData: [🕒VersionFormat] = [
     ("1.2","""
             Third release
-            
             3回目のリリース
             ""","2022-03-01"),
     ("1.1","""
             Second release
-            
             2回目のリリース
             ""","2022-02-01"),
     ("1.0","""
             Initial release
-            
             最初のリリース
             ""","2022-01-01")]
