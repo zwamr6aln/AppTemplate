dcec11d3d8b4dbf02d16215b1b11475cb71d2d2a
aaaa <aaaa>
2022-08-03 17:10:40 +0900

    iap

--- AppTemplate/ContentView.swift
+++ AppTemplate/ContentView.swift
@@ -8,6 +8,9 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
+                Text("🛒.🚩PurchasedADFree")
+                    .badge(🛒.🚩PurchasedADFree.description)
+                
                 Section { 🛠MenuButton() }
                 
                 🔗OpenSystemSetting()
