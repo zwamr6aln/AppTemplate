ebd8d6dec4850f651cd97420550858d47287f08d
aaaaa <aaaaa>
2022-11-13 19:27:15 +0900

    Add share extension

---
+++ ShareExtension/ShareViewController.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        print(#function)
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        print(#function)
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        print("📠: ", contentText.debugDescription)
+    }
+
+    override func configurationItems() -> [Any]! {
+        print(#function)
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
