70f74b3624ab45d43a474d441c3d0a2fb5de5e6a
aaaaa <aaaaa>
2022-11-13 19:20:33 +0900

    defaultAppStorage

--- AppTemplate/AppTemplateApp.swift
+++ AppTemplate/AppTemplateApp.swift
@@ -6,12 +6,14 @@ struct AppTemplateApp: App {
     @UIApplicationDelegateAdaptor private var ⓓelegate: 🄰pplicationDelegate
     @StateObject private var 📱 = 📱AppModel()
     @StateObject private var 🛒 = 🛒StoreModel(id: "PLACEHOLDER.adfree")
+    let 💾 = UserDefaults(suiteName: "group.net.aaaakkkkssssttttnnnn.AppTemplate") ?? UserDefaults()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(📱)
                 .environmentObject(🛒)
+                .defaultAppStorage(💾)
         }
     }
 }
