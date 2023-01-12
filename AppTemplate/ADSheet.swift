f5503bbac6b93df068aeae0b633391852987cd9c
aaaaa <aaaaa>
2023-01-12 14:50:56 +0900

    a

--- AppTemplate/ADSheet.swift
+++ AppTemplate/ADSheet.swift
@@ -5,43 +5,12 @@ struct ADSheet: View {
     @Environment(\.verticalSizeClass) var verticalSizeClass
     var body: some View {
         NavigationView {
-            if verticalSizeClass == .regular {
-                VStack(spacing: 18) {
-                    Image(systemName: "photo")
-                        .resizable()
-                        .scaledToFit()
-                    Spacer()
-                    Image(systemName: "square")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                    Text("App name")
-                        .font(.headline)
-                    Text("This is placeholder. Please deploy app description. 2 ~ 3 lines.")
-                        .font(.subheadline)
-                        .multilineTextAlignment(.center)
-                    Spacer()
-                    HStack(spacing: 6) {
-                        Image("appstore_badge")
-                        Image(systemName: "hand.point.up.left")
-                    }
-                    .padding()
-                }
-                .padding()
-                .navigationTitle("AD")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Image(systemName: "questionmark.circle")
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Image(systemName: "chevron.down")
-                    }
-                }
-            } else {
-                HStack(spacing: 16) {
-                    Image(systemName: "photo")
-                        .resizable()
-                        .scaledToFit()
+            Group {
+                if self.verticalSizeClass == .regular {
                     VStack(spacing: 18) {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
                         Spacer()
                         Image(systemName: "square")
                             .resizable()
@@ -58,17 +27,41 @@ struct ADSheet: View {
                         }
                         .padding()
                     }
-                    .padding(.horizontal)
+                    .padding()
+                } else {
+                    HStack(spacing: 16) {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                        VStack(spacing: 18) {
+                            Spacer()
+                            Image(systemName: "square")
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                            Text("App name")
+                                .font(.headline)
+                            Text("This is placeholder. Please deploy app description. 2 ~ 3 lines.")
+                                .font(.subheadline)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            HStack(spacing: 6) {
+                                Image("appstore_badge")
+                                Image(systemName: "hand.point.up.left")
+                            }
+                            .padding()
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding()
                 }
-                .padding()
-                .navigationTitle("AD")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Image(systemName: "questionmark.circle")
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Image(systemName: "chevron.down")
-                    }
+            }
+            .navigationTitle("AD")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image(systemName: "questionmark.circle")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "chevron.down")
                 }
             }
         }
@@ -80,6 +73,8 @@ struct ADSheet: View {
 struct View_Previews: PreviewProvider {
     static var previews: some View {
         ADSheet()
-            //.previewLayout(.fixed(width: 300, height: 600))
+            .previewInterfaceOrientation(.portrait)
+        ADSheet()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
