bcabe7284fd43edfa1d12ddadb551128929d580c
aaaaa <aaaaa>
2023-01-26 12:33:52 +0900

    a

--- AppTemplate/🧰Others/📣AD.swift
+++ AppTemplate/🧰Others/📣AD.swift
@@ -214,7 +214,7 @@ enum 📣MyApp: String, CaseIterable {
         switch self {
             case .FlipByBlink: return "Simple and normal ebook reader (for fixed-layout). Only a special feature. Turn a page with slightly longish voluntary blink."
             case .FadeInAlarm: return "Alarm clock with taking a long time from small volume to max volume."
-            case .PlainShogiBoard: return "Simple Shogi board App. Based on iOS system UI design."
+            case .PlainShogiBoard: return "Simple Shogi board App. Based on iOS system UI design. Supported SharePlay."
             case .TapWeight: return "Register weight data to the Apple \"Health\" application pre-installed on iPhone in the fastest possible way (as manual)."
             case .TapTemperature: return "Register body temperature data to the \"Health\" app pre-installed on iPhone in the fastest possible way (as manual)."
             case .MemorizeWidget: return "Flashcard on widget. Memorize a note in everyday life."
