e0c86159152a5ea9ed359d10852bd652760b0e5f
aaaaa <aaaaa>
2023-02-26 12:27:21 +0900

    a

--- AppTemplate/💬RequestUserReview.swift
+++ AppTemplate/💬RequestUserReview.swift
@@ -12,38 +12,3 @@ struct 💬RequestUserReviewOnLaunch: ViewModifier {//アプリ毎に個別に�
             .onAppear { self.checkToRequest = true }
     }
 }
-
-struct 💬RequestUserReview: ViewModifier {
-    @Binding private var checkToRequest: Bool
-    func body(content: Content) -> some View {
-        if #available(iOS 16.0, *) {
-            content
-                .modifier(ⓜodifier(self.$checkToRequest))
-        } else {
-            content
-        }
-    }
-    init(_ checkToRequest: Binding<Bool>) {
-        self._checkToRequest = checkToRequest
-    }
-    @available(iOS 16, *)
-    private struct ⓜodifier: ViewModifier {
-        @Environment(\.requestReview) var requestReview
-        @AppStorage("launchCount") private var ⓛaunchCount: Int = 0
-        @Binding private var checkToRequest: Bool
-        func body(content: Content) -> some View {
-            content
-                .task { self.ⓛaunchCount += 1 }
-                .onChange(of: self.checkToRequest) {
-                    if $0 == true {
-                        if [10, 30, 60, 100].contains(self.ⓛaunchCount) {
-                            self.requestReview()
-                        }
-                    }
-                }
-        }
-        init(_ checkToRequest: Binding<Bool>) {
-            self._checkToRequest = checkToRequest
-        }
-    }
-}
