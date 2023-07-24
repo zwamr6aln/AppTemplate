adf51f492ce23db0f9b72df7a19e7b6af4b03cd6
aaaaa <aaaaa>
2023-07-24 12:41:22 +0900

    a

--- macOS/🪄Commands.swift
+++ macOS/🪄Commands.swift
@@ -2,10 +2,11 @@ import SwiftUI
 
 struct 🪄Commands: Commands {
     var body: some Commands {
-        SidebarCommands()
-        CommandGroup(replacing: .undoRedo) { EmptyView() }
         CommandGroup(replacing: .systemServices) { EmptyView() }
+        CommandGroup(replacing: .undoRedo) { EmptyView() }
         CommandGroup(replacing: .pasteboard) { EmptyView() }
+        SidebarCommands()
+        CommandGroup(replacing: .help) { EmptyView() }
         CommandGroup(after: .help) {
             Link("Link", destination: URL(string: "https://apple.com")!)
         }
