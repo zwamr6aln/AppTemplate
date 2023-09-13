331314e0ddab2b3b05a05868714e308ad9c9c8d2
aaaaa <aaaaa>
2023-09-13 17:03:25 +0900

    a

--- iOS/App.swift
+++ iOS/App.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @main
 struct AppTemplate: App {
     private let 📱 = 📱AppModel()
-    private let 🛒 = 🛒InAppPurchaseModel(id: "PLACEHOLDER2.adfree")
+    private let 🛒 = 🛒InAppPurchaseModel(id: "PLACEHOLDER.adfree")
     var body: some Scene {
         WindowGroup {
             ContentView()
