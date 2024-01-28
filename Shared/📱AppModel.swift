01c71603d439ee1c362211c06f4b33414bb1c932
aaaa <a@a>
2024-01-28 14:38:19 +0900

    a

--- Shared/📱AppModel.swift
+++ Shared/📱AppModel.swift
@@ -6,6 +6,6 @@ class 📱AppModel: NSObject, ObservableObject {
 #endif
     
 #if os(iOS) || os(macOS) || os(visionOS)
-    let inAppPurchaseModel: 🛒InAppPurchaseModel = .init(id: "PLACEHOLDER.adfree")
+    let inAppPurchaseModel: 🛒InAppPurchaseModel = .init(id: "PLACEHOLDER2.adfree")
 #endif
 }
